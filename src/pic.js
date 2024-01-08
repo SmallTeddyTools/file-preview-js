6655c047b27b6358d0b41c5fb77fc75a0035263a
SmallTeddy <zhangzeming1212@gmail.com>
2024-01-08 16:27:23 +0800

    fix: test utils function

--- src/pic.js
+++ src/pic.js
@@ -2,20 +2,19 @@
 console.log("preview jpg/jpeg/png onload ok")
 
 window.onload = function () {
-  var result = document.getElementById("result");
-  var file = document.getElementById("file")
+  const file = document.querySelector("#file")
   if (typeof FileReader == "undefined") {
     file.setAttribute("disabled", "disabled")
-    return console.error('不支持!')
+    return console.error("不支持!")
   }
 }
 
 function readAsDataURL() {
   //文件以Data URL形式进行读入页面
-  const file = document.querySelector('#file').files[0]
+  const file = document.querySelector("#file").files[0]
   console.log(file.type)
   if (!file.type.startsWith("image")) {
-    return console.error('请提交图片!')
+    return console.error("请提交图片!")
   }
   const reader = new FileReader()
   reader.readAsDataURL(file)
--- src/utils.js
+++ src/utils.js
@@ -6,14 +6,14 @@ const showFileInfo = () => {
 }
 
 function readAsText() {
-  const file = document.querySelector('#file').files[0]
+  const file = document.querySelector("#file").files[0]
   const reader = new FileReader()
   reader.readAsText(file)
   reader.onload = e => result.innerHTML = e.target.result
 }
 
 function readAsBinaryString() {
-  const file = document.querySelector('#file').files[0]
+  const file = document.querySelector("#file").files[0]
   const reader = new FileReader()
   reader.readAsBinaryString(file)
   reader.onload = e => result.innerHTML = e.target.result
@@ -32,53 +32,62 @@ function TextEncoder2ArrayBuffer(text) {
   const uint8Array = encoder.encode(text);
   // Uint8Array 转 ArrayBuffer
   const arrayBuffer = uint8Array.buffer
-  return arrayBuffer
+  result.innerHTML = uint8Array
+  alert("数据在控制台查看")
+  console.log(arrayBuffer)
 }
-
-console.log(TextEncoder2ArrayBuffer('hello world'))
-// ArrayBuffer {
-//   [Uint8Contents]: <68 65 6c 6c 6f 20 77 6f 72 6c 64>,
-//   byteLength: 11
-// }
 
 /**
  * 用于将 blob 转换为 ArrayBuffer
- * @param {blob} str 
  */
-function blob2ArrayBuffer(str) {
-  // 创建 Blob 对象，指定类型为 text/plain;charset=utf-8
-  const blob = new Blob([str], { type: 'text/plain;charset=utf-8' });
-  // 创建 TextDecoder 对象
-  const utf8decoder = new TextDecoder()
-  // 获取 ArrayBuffer
-  blob.arrayBuffer().then(buffer => {
-    // 创建 ArrayBuffer
-    const text = utf8decoder.decode(buffer)
-    // 返回解码后的字符串
-    return text
-  })
+function blob2ArrayBuffer() {
+  let str = ''
+  const file = document.querySelector("#file").files[0]
+  const reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onload = e => {
+    str = e.target.result
+    // 创建 Blob 对象，指定类型为 text/plain;charset=utf-8
+    const blob = new Blob([str], { type: "text/plain;charset=utf-8" });
+    // 创建 TextDecoder 对象
+    const utf8decoder = new TextDecoder()
+    // 获取 ArrayBuffer
+    blob.arrayBuffer().then(buffer => {
+      // 创建 ArrayBuffer
+      const text = utf8decoder.decode(buffer)
+      // 返回解码后的字符串
+      result.innerHTML = text
+    })
+  }
+
 }
 
 /**
  * 创建一个函数，用于将字符串转换为ArrayBuffer
- * @param {blob} str 
  */
-function fileReader2ArrayBuffer(str) {
-  // 创建一个blob对象，用于存储字符串
-  const blob = new Blob([str], { type: 'text/plain;charset=utf-8' });
-  // 创建一个TextDecoder对象，用于解码blob对象
-  const utf8decoder = new TextDecoder()
-  // 创建一个FileReader对象，用于读取blob对象
-  const fr = new FileReader()
-  // 读取blob对象
-  fr.readAsArrayBuffer(blob)
-  // 当blob对象读取完毕时，执行回调函数
-  fr.onload = function (res) {
-    // 创建 ArrayBuffer
-    const buffer = fr.result
-    // 使用TextDecoder解码ArrayBuffer
-    const text = utf8decoder.decode(buffer)
-    // 返回解码后的字符串
-    return text
+function fileReader2ArrayBuffer() {
+  let str = ''
+  const file = document.querySelector("#file").files[0]
+  const reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onload = e => {
+    str = e.target.result
+    // 创建一个blob对象，用于存储字符串
+    const blob = new Blob([str], { type: "text/plain;charset=utf-8" });
+    // 创建一个TextDecoder对象，用于解码blob对象
+    const utf8decoder = new TextDecoder()
+    // 创建一个FileReader对象，用于读取blob对象
+    const fr = new FileReader()
+    // 读取blob对象
+    fr.readAsArrayBuffer(blob)
+    // 当blob对象读取完毕时，执行回调函数
+    fr.onload = () => {
+      // 创建 ArrayBuffer
+      const buffer = fr.result
+      // 使用TextDecoder解码ArrayBuffer
+      const text = utf8decoder.decode(buffer)
+      // 返回解码后的字符串
+      result.innerHTML = text
+    }
   }
 }
